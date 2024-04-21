4046138066dbd3881056ad405125f6de85071d59
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-04-21 09:52:04 +0700

    update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,12 +3,12 @@ import type { StorybookConfig } from '@storybook/react-webpack5'
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
         '@storybook/preset-create-react-app',
         '@storybook/addon-onboarding',
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@chromatic-com/storybook',
         '@storybook/addon-interactions',
-        '@storybook/addon-styling-webpack',
     ],
     framework: {
         name: '@storybook/react-webpack5',
@@ -17,6 +17,6 @@ const config: StorybookConfig = {
     docs: {
         autodocs: 'tag',
     },
-    staticDirs: ['..\\public'],
+    staticDirs: ['../public'],
 }
 export default config
